845e0b4a6de9297452e7e3f194b8cc0a51c6aae2
Christian Galla <ChristianGalla@users.noreply.github.com>
2020-03-07 12:16:11 +0100

    Added Notifications (WIP)

---
+++ AutoStartConfirm/DesktopNotifications/MyNotificationActivator.cs
@@ -0,0 +1,20 @@
+﻿using DesktopNotifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static DesktopNotifications.NotificationActivator;
+
+namespace AutoStartConfirm.DesktopNotifications {
+    // The GUID CLSID must be unique to your app. Create a new GUID if copying this code.
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(INotificationActivationCallback))]
+    [Guid("DF651CDF-EC4A-4C98-9419-DE2709B00863"), ComVisible(true)]
+    public class MyNotificationActivator : NotificationActivator {
+        public override void OnActivated(string invokedArgs, NotificationUserInput userInput, string appUserModelId) {
+            // TODO: Handle activation
+        }
+    }
+}
