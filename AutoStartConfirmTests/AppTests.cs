b99e2709fffe127fd4ba8b024b60e6003c8d2beb
Christian Galla <ChristianGalla@users.noreply.github.com>
2020-08-15 19:06:09 +0200

    Created test project

---
+++ AutoStartConfirmTests/AppTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoStartConfirm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoStartConfirm.Tests {
+    [TestClass()]
+    public class AppTests {
+        //[TestMethod()]
+        //public void AppStarts() {
+        //    var app = new App();
+        //    // Assert.Fail();
+        //}
+    }
+}
