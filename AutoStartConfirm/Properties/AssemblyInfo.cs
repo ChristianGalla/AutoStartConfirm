f3b599f22fb5e198df08721bb6567151b06a199c
Christian Galla <ChristianGalla@users.noreply.github.com>
2021-10-15 20:59:11 +0200

    Bumped version number to 1.0

--- AutoStartConfirm/Properties/AssemblyInfo.cs
+++ AutoStartConfirm/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("1.0.*")]
