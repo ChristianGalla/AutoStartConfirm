3f02d80156596e2858ce7730c8560c32f1377473
Christian Galla <ChristianGalla@users.noreply.github.com>
2022-02-19 18:15:52 +0100

    Started move to WinUI 3

--- AutoStartConfirmLib/Connectors/Services/OtherServiceConnector.cs
+++ AutoStartConfirmLib/Connectors/Services/OtherServiceConnector.cs
@@ -1,11 +1,5 @@
 ﻿using AutoStartConfirm.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Management;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoStartConfirm.Connectors.Services {
     public class OtherServiceConnector : ServiceConnector {
