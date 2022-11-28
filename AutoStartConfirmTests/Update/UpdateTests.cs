537a1340d674f3a263f79a888463ee1316a4f21d
Christian Galla <ChristianGalla@users.noreply.github.com>
2022-11-28 22:13:25 +0100

    Fixed unit tests

--- AutoStartConfirmTests/Update/UpdateTests.cs
+++ AutoStartConfirmTests/Update/UpdateTests.cs
@@ -5,6 +5,11 @@ using FakeItEasy;
 using AutoStartConfirm.Notifications;
 using Semver;
 using AutoStartConfirm.Properties;
+using Octokit;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Security.Policy;
 
 namespace AutoStartConfirm.Update.Tests
 {
@@ -12,9 +17,51 @@ namespace AutoStartConfirm.Update.Tests
     public class UpdateTests
     {
 
-        private UpdateService Service = new UpdateService();
-        INotificationService NotificationService = A.Fake<INotificationService>();
-        ISettingsService SettingsService = A.Fake<ISettingsService>();
+        UpdateService Service = new UpdateService();
+        static INotificationService NotificationService = A.Fake<INotificationService>();
+        static ISettingsService SettingsService = A.Fake<ISettingsService>();
+        static IGitHubClient GitHubClient = A.Fake<IGitHubClient>();
+        static string CurrentVersion = "1.0.0";
+        static string NewestVersion = "2.0.0";
+        static string Url = "https://www.example.org";
+        static string MsiUrl = "https://www.example.org/AutoStartConfirm.msi";
+        static List<ReleaseAsset> ReleaseAssets = new List<ReleaseAsset>()
+        {
+            new ReleaseAsset(
+                url: MsiUrl,
+                id: 1,
+                nodeId: "",
+                name: "AutoStartConfirm.msi",
+                label: "",
+                state: "",
+                contentType: "",
+                size: 1,
+                downloadCount: 1,
+                createdAt: new DateTimeOffset(),
+                updatedAt: new DateTimeOffset(),
+                browserDownloadUrl: MsiUrl,
+                uploader: null)
+        };
+        static Release Release = new Release(
+            url: Url,
+            htmlUrl: Url,
+            assetsUrl: Url,
+            uploadUrl: Url,
+            id: 1,
+            nodeId: "",
+            tagName: NewestVersion,
+            targetCommitish: "",
+            name: NewestVersion,
+            body: "",
+            draft: false,
+            prerelease: false,
+            createdAt: new DateTimeOffset(),
+            publishedAt: new DateTimeOffset(),
+            author: null,
+            tarballUrl: Url,
+            zipballUrl: Url,
+            assets: ReleaseAssets
+        );
 
         [TestInitialize]
         public void TestInitialize()
@@ -23,39 +70,37 @@ namespace AutoStartConfirm.Update.Tests
             Fake.ClearRecordedCalls(NotificationService);
             Service.NotificationService = NotificationService;
             Service.SettingsService = SettingsService;
+            Service.GitHubClient = GitHubClient;
+            Service.CurrentVersion = new SemVersion(1, 0, 0);
+            
+            A.CallTo(() => GitHubClient.Repository.Release.GetLatest("ChristianGalla", "AutoStartConfirm")).Returns(Release);
         }
 
         [TestMethod()]
         public async Task CheckUpdateAndShowNotification_ShowsNotificationIfUpdateAavailable()
         {
-            string currentVersion = "1.0.0";
-            string newestVersion = "2.0.0";
-            Service.CurrentVersion = new SemVersion(1, 0, 0);
-            Service.NewestVersion = Task.Run(() => new SemVersion(2, 0, 0));
             await Service.CheckUpdateAndShowNotification();
 
-            A.CallTo(() => NotificationService.ShowNewVersionNotification(newestVersion, currentVersion)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => NotificationService.ShowNewVersionNotification(NewestVersion, CurrentVersion, MsiUrl)).MustHaveHappenedOnceExactly();
         }
 
         [TestMethod()]
         public async Task CheckUpdateAndShowNotification_ShouldNotShowNotificationIfNoUpdateAavailable()
         {
-            Service.CurrentVersion = new SemVersion(1, 0, 0);
-            Service.NewestVersion = Task.Run(() => new SemVersion(1, 0, 0));
+            Service.CurrentVersion = new SemVersion(2, 0, 0);
             await Service.CheckUpdateAndShowNotification();
 
-            A.CallTo(() => NotificationService.ShowNewVersionNotification(A<string>._, A<string>._)).MustNotHaveHappened();
+            A.CallTo(() => NotificationService.ShowNewVersionNotification(A<string>._, A<string>._, A<string>._)).MustNotHaveHappened();
         }
 
         [TestMethod()]
         public async Task CheckUpdateAndShowNotification_ShouldNotShowNotificationIfAlreadyShown()
         {
             Service.CurrentVersion = new SemVersion(1, 0, 0);
-            Service.NewestVersion = Task.Run(() => new SemVersion(2, 0, 0));
             A.CallTo(() => SettingsService.LastNotifiedNewVersion).Returns("2.0.0");
             await Service.CheckUpdateAndShowNotification();
 
-            A.CallTo(() => NotificationService.ShowNewVersionNotification(A<string>._, A<string>._)).MustNotHaveHappened();
+            A.CallTo(() => NotificationService.ShowNewVersionNotification(A<string>._, A<string>._, A<string>._)).MustNotHaveHappened();
         }
     }
 }
