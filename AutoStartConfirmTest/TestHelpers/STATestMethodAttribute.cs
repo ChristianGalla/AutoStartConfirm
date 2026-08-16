db38621d9be9c5fec1ede79fcfeaf09e45053eb7
Christian Galla <ChristianGalla@users.noreply.github.com>
2026-08-16 13:12:53 +0200

    test: fix unit test to run on newest MSTest.TestFramework version

--- AutoStartConfirmTest/TestHelpers/STATestMethodAttribute.cs
+++ AutoStartConfirmTest/TestHelpers/STATestMethodAttribute.cs
@@ -1,36 +1,38 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AutoStartConfirmTests.TestHelpers
 {
-    public class STATestMethodAttribute : TestMethodAttribute
+    public class STATestMethodAttribute(
+        TestMethodAttribute? testMethodAttribute,
+#pragma warning disable CS9113 // Parameter is unread.
+        [CallerFilePath] string? declaringFilePath = "",
+        [CallerLineNumber] int declaringLineNumber = 0
+#pragma warning restore CS9113 // Parameter is unread.
+        ) : TestMethodAttribute
     {
-        private readonly TestMethodAttribute? _testMethodAttribute;
-
-        public STATestMethodAttribute(TestMethodAttribute? testMethodAttribute)
-        {
-            _testMethodAttribute = testMethodAttribute;
-        }
-
-        public override TestResult[] Execute(ITestMethod testMethod)
+        public override async Task<TestResult[]> ExecuteAsync(ITestMethod testMethod)
         {
             if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
-                return Invoke(testMethod);
+                return await InvokeAsync(testMethod).ConfigureAwait(false);
 
             TestResult[]? result = null;
-            var thread = new Thread(() => result = Invoke(testMethod));
+            var thread = new Thread(() => result = InvokeAsync(testMethod).GetAwaiter().GetResult());
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
             thread.Join();
             return result!;
         }
 
-        private TestResult[] Invoke(ITestMethod testMethod)
+        private async Task<TestResult[]> InvokeAsync(ITestMethod testMethod)
         {
-            if (_testMethodAttribute != null)
-                return _testMethodAttribute.Execute(testMethod);
+            if (testMethodAttribute != null)
+                return await testMethodAttribute.ExecuteAsync(testMethod).ConfigureAwait(false);
 
-            return new[] { testMethod.Invoke(null) };
+            var testResult = await testMethod.InvokeAsync(null).ConfigureAwait(false);
+            return [testResult];
         }
     }
 }
